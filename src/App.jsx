172dd053d258ac2d3291aaccfc16eb516e371f28
Yann-Donald <yanndonald237@gmail.com>
2024-10-16 14:23:22 +0100

    added and implemented login state chane using zustand

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,28 @@ import Login from "./components/login/Login";
 import Notifications from "./components/notifications/Notifications";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./lib/firebase";
+import { useUserStore } from "./lib/userStore";
 
 function App() {
-  const user = false;
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
 
   useEffect(() => {
     const unSub = onAuthStateChanged(auth, (user) => {
-      console.log(user);
+      fetchUserInfo(user.uid);
     });
 
     return () => {
       unSub();
     };
-  }, []);
+  }, [fetchUserInfo]);
+
+  if (isLoading) return <div className="loading">Loading...</div>;
 
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="container w-[180vh] h-[90vh] bg-slate-800/70 border border-white/20 backdrop-blur-lg mt-10 rounded-lg text-white overflow-hidden">
         <div className="flex h-full">
-          {user ? (
+          {currentUser ? (
             <>
               <List />
               <Chat />
